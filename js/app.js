aaeb7d250634c4d4f39cb98c3b5494fb50eac0b7
Abygaelle <abygaelleb02@gmail.com>
2022-02-03 00:52:00 -0500

    step 6'

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const btn = document.querySelector('button')
 
+btn.addEventListener('click', function(evt){
+  const int = document.querySelector('input')
+  
 
-const int = document.querySelector('input')
+  const unl = document.querySelector('ul')
+  const newLi = document.createElement('li')
+
+  newLi.textContent = int.value
+  int.value = ''
+  console.log(newLi)
+})
 
 
-const unl = document.querySelector('ul')
+
+
+
 
