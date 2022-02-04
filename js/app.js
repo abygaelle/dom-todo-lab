4f0baa6821fe31905737d3397a038a7aa658b1ee
Abygaelle <abygaelleb02@gmail.com>
2022-02-04 00:10:17 -0500

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 const btn = document.querySelector('button')
+const unl = document.querySelector('ul')
 
 btn.addEventListener('click', function(evt){
+  const newLi = document.createElement('li')
   const int = document.querySelector('input')
   
-
-  const unl = document.querySelector('ul')
-  const newLi = document.createElement('li')
-
   newLi.textContent = int.value
-  int.value = ''
-  console.log(newLi)
+
+  if (inp.value !== ""){
+    document.querySelector('ul').appendChild(newLi)
+    inp.value = ""
+    console.log(newLi)
+  }
+  
 })
+
 
 
 
